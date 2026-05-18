9c78d27d7c934d065e465009811cb85b88826586
Stanislaw Wojciechowski <stanislaw.wojciechowski@epita.fr>
2026-05-18 14:43:51 +0200

    database corpus table

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -14,6 +14,20 @@ chunk_order int NOT NULL,
 chunk_text text NOT NULL,
 FOREIGN KEY (file_id) REFERENCES documents(document_id)
 );
+CREATE TABLE  corpus(
+corpus_id int PRIMARY KEY AUTO_INCREMENT,
+corpus_name varchar(255) NOT NULL,
+upload_date datetime NOT NULL,
+corpus_desc text
+);
+CREATE TABLE corpus_documents(
+document_id int,
+corpus_id   int,
+PRIMARY KEY(document_id, corpus_id),
+FOREIGN KEY(document_id) REFERENCES documents(document_id),
+FOREIGN KEY(corpus_id) REFERENCES corpus(corpus_id)
+);
+
 
 
 
