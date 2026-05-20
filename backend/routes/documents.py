0682e8dd44238b5d4ebe3863188cc20fd2b7831a
Stanislaw Wojciechowski <stanislaw.wojciechowski@epita.fr>
2026-05-20 21:01:00 +0200

    document route metadata save

--- backend/routes/documents.py
+++ backend/routes/documents.py
@@ -1,3 +1,5 @@
+import os 
+import datetime
 from flask import Blueprint, jsonify, request
 from config import UPLOAD_FOLDER
 documents_bp = Blueprint('documents',__name__)
@@ -6,5 +8,9 @@ def add_file():
     file = request.files['file']
     filename = file.filename
     file.save(f'{UPLOAD_FOLDER}/{filename}')
+    size = os.path.getsize(f'{UPLOAD_FOLDER}/{filename}')
+    _, document_type = os.path.splitext(filename)
+    uploaded_at = datetime.datime.now()
     return jsonify({'status': 'ok', 'filename': filename})
+
     
