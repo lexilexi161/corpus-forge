9b8f883bdba6b0deb83b74fb8015baaa05b1deaa
Stanislaw Wojciechowski <stanislaw.wojciechowski@epita.fr>
2026-05-20 20:45:38 +0200

    config.py

--- backend/config.py
+++ backend/config.py
@@ -1,1 +1,6 @@
+import os
+GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
 UPLOAD_FOLDER = 'uploads'
+DATABASE_PATH = 'database/corpus-forge.db'
+FLASK_SECRET_KEY = 'X'
+DEBUG_MODE = True
--- backend/routes/cost.py
+++ backend/routes/cost.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 cost_bp = Blueprint('cost',__name__)
-@cost_bp.route('/cost',methods = ['GET'])
+@cost_bp.route('/cost', methods = ['GET'])
 def get_cost():
     return jsonify({'total_requests' : 0,
         'prompt_tokens' : 0,
