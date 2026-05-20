eda2750dcb09c9ea084a7f16cc7a9ea4337e716d
Stanislaw Wojciechowski <stanislaw.wojciechowski@epita.fr>
2026-05-20 23:07:45 +0200

    database shcema cleanup

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,69 +1,65 @@
 CREATE TABLE documents (
-document_id int PRIMARY KEY AUTO_INCREMENT,
-document_name varchar(255) NOT NULL,
-size int NOT NULL,
-uploaded_at datetime NOT NULL,
-document_type varchar(255) NOT NULL,
-file_path varchar(255) NOT NULL
+document_id INTEGER PRIMARY KEY AUTOINCREMENT,
+document_name VARCHAR(255) NOT NULL,
+size INTEGER NOT NULL,
+uploaded_at DATETIME NOT NULL,
+document_type VARCHAR(255) NOT NULL,
+file_path VARCHAR(255) NOT NULL
 );
 CREATE TABLE chunks (
-file_id int,
-id int PRIMARY KEY AUTO_INCREMENT, 
-embedding text NOT NULL,
-chunk_order int NOT NULL,
-chunk_text text NOT NULL,
+file_id INTEGER,
+id INTEGER PRIMARY KEY AUTOINCREMENT, 
+embedding TEXT NOT NULL,
+chunk_order INTEGER NOT NULL,
+chunk_text TEXT NOT NULL,
 FOREIGN KEY (file_id) REFERENCES documents(document_id)
 );
-CREATE TABLE  corpus(
-corpus_id int PRIMARY KEY AUTO_INCREMENT,
-corpus_name varchar(255) NOT NULL,
-upload_date datetime NOT NULL,
-corpus_desc text
+CREATE TABLE corpus(
+corpus_id INTEGER PRIMARY KEY AUTOINCREMENT,
+corpus_name VARCHAR(255) NOT NULL,
+upload_date DATETIME NOT NULL,
+corpus_desc TEXT
 );
 CREATE TABLE corpus_documents(
-document_id int,
-corpus_id   int,
+document_id INTEGER,
+corpus_id INTEGER,
 PRIMARY KEY(document_id, corpus_id),
 FOREIGN KEY(document_id) REFERENCES documents(document_id),
 FOREIGN KEY(corpus_id) REFERENCES corpus(corpus_id)
 );
 CREATE TABLE artifacts(
-artifact_id int PRIMARY KEY AUTO_INCREMENT,
-artifact_name varchar(255) NOT NULL,
-artifact_date datetime NOT NULL,
-artifact_size int NOT NULL,
-artifact_type varchar(255) NOT NULL,
-artifact_path varchar(255) NOT NULL,
-artifact_prompt text,
-corpus_id int,
-document_id int, 
+artifact_id INTEGER PRIMARY KEY AUTOINCREMENT,
+artifact_name VARCHAR(255) NOT NULL,
+artifact_date DATETIME NOT NULL,
+artifact_size INTEGER NOT NULL,
+artifact_type VARCHAR(255) NOT NULL,
+artifact_path VARCHAR(255) NOT NULL,
+artifact_prompt TEXT,
+corpus_id INTEGER,
+document_id INTEGER,
 FOREIGN KEY(corpus_id) REFERENCES corpus(corpus_id),
 FOREIGN KEY(document_id) REFERENCES documents(document_id)
 );
 CREATE TABLE user(
-user_id int PRIMARY KEY AUTO_INCREMENT,
-username varchar(255) NOT NULL,
-user_password varchar(255) NOT NULL,
-date_created datetime NOT NULL
+user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+username VARCHAR(255) NOT NULL,
+user_password VARCHAR(255) NOT NULL,
+date_created DATETIME NOT NULL
 );
 CREATE TABLE session(
-session_id INT PRIMARY KEY AUTO_INCREMENT,
-user_id INT,
-session_token varchar(255) NOT NULL,
-started_at datetime NOT NULL,
-ended_at datetime ,
+session_id INTEGER PRIMARY KEY AUTOINCREMENT,
+user_id INTEGER,
+session_token VARCHAR(255) NOT NULL,
+started_at DATETIME NOT NULL,
+ended_at DATETIME,
 FOREIGN KEY(user_id) REFERENCES user(user_id)
 );
 CREATE TABLE cost(
-request_id int PRIMARY KEY AUTO_INCREMENT,
-user_id int, 
-request_type varchar(255) NOT NULL,
-input_tokens int(255),
-output_tokens int(255),
-created_at datetime NOT NULL,
-FOREIGN KEY(user_id) REFERENCEs user(user_id)
+request_id INTEGER PRIMARY KEY AUTOINCREMENT,
+user_id INTEGER,
+request_type VARCHAR(255) NOT NULL,
+input_tokens INTEGER,
+output_tokens INTEGER,
+created_at DATETIME NOT NULL,
+FOREIGN KEY(user_id) REFERENCES user(user_id)
 );
-
-
-
-
