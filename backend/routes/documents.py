b9cb4069ea0e744816fcf24b13b75052f9bdaaa8
Stanislaw Wojciechowski <stanislaw.wojciechowski@epita.fr>
2026-05-20 22:13:22 +0200

    documents.py full

--- backend/routes/documents.py
+++ backend/routes/documents.py
@@ -1,5 +1,6 @@
 import os 
 import datetime
+from models.db import get_connection
 from flask import Blueprint, jsonify, request
 from config import UPLOAD_FOLDER
 documents_bp = Blueprint('documents',__name__)
@@ -10,7 +11,11 @@ def add_file():
     file.save(f'{UPLOAD_FOLDER}/{filename}')
     size = os.path.getsize(f'{UPLOAD_FOLDER}/{filename}')
     _, document_type = os.path.splitext(filename)
-    uploaded_at = datetime.datime.now()
-    return jsonify({'status': 'ok', 'filename': filename})
-
-    
+    uploaded_at = datetime.datetime.now()
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO documents(document_name, size, uploaded_at, document_type, file_path) " \
+    "VALUES(?, ?, ?, ?, ? )", (filename, size , uploaded_at, document_type, f'{UPLOAD_FOLDER}/{filename}' ) )
+    conn.commit()
+    uploaded_at_string = uploaded_at.isoformat()
+    return jsonify({'status': 'ok', 'filename': filename, 'size': size,'uploaded_at': uploaded_at_string,'document_Type': document_type, 'filepath':f'{UPLOAD_FOLDER}/{filename}'  })
